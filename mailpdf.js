7aa3c16fd7e385bc3e189a7278d9ae4f1c81bcc5
Marc Portier <MPT@w272.westtoer.be>
2015-07-01 12:20:29 +0200

    process real data

--- mailpdf.js
+++ mailpdf.js
@@ -13,14 +13,22 @@ var data2pdf = require('./lib/data2pdf'),
         .describe('input', 'Location of data.csv to work with')
         .alias('i', 'input')
         .default('input', path.join(__dirname, 'data', 'test', 'data.csv'))
+        .describe('ymonth', 'YYYYMM code of the data to be processed')
+        .alias('y', 'ymonth')
+
         .describe('mailfield', 'name of the email-field in the csv')
         .alias('m', 'mailfield')
         .default('mailfield', 'email')
+        .describe('subject', 'subject for the email to use')
+        .alias('s', 'subject')
+        .default('subject', 'Westtoer nota vrijwilligerswerk (%ymonth%)')
         .describe('nomail', 'don\'t send email, just produce pdf and end')
         .alias('n', 'nomail')
         .describe('output', 'location for the pddf to be stored')
         .alias('o', 'output')
         .default('o', path.join('tmp', 'mkpdf', 'output', 'test'))
+
+        .demand(["ymonth"])
         .argv;
 
 data2pdf.doIt(argv);
